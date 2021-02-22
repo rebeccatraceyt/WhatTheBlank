902b75b3e89dc7651a035fd78d1268da68799e25
Rebecca Tracey-Timoney <71783662+rebeccatraceyt@users.noreply.github.com>
2021-02-22 22:11:41 +0000

    Fixed Sliding Footer bug

--- assets/js/script.js
+++ assets/js/script.js
@@ -18,18 +18,18 @@
 document.getElementById("currentYear").innerHTML = new Date().getFullYear();
 
 // Implements sliding footer function
-    // sourced and edited from http://jsfiddle.net/nathanbweb/JHu7j/ 
-jQuery(function($) {
-    var open = false;
-    $('#footerButton').click(function () {
-        if(open === false) {
-            $('#footerContent').animate({ height: '300px' });
-            $(this).css('backgroundPosition', 'bottom left');
-            open = true;
-        } else {
-            $('#footerContent').animate({ height: '0px' });
-            $(this).css('backgroundPosition', 'top left');
-            open = false;
-        }
-    });		
-});
+    // sourced and edited from http://jsfiddle.net/nathanbweb/JHu7j/     
+var open = false;
+$('#footerButton').click(function () {
+    if(open === false) {
+        $('#footerContent').animate({ height: '30px' });
+        $(this).css('backgroundPosition', 'bottom left');
+        $("i", this).toggleClass("fa-caret-square-up fa-caret-square-down") // reference: https://stackoverflow.com/questions/15345784/change-icon-on-click-toggle/15345885 
+        open = true;
+    } else {
+        $('#footerContent').animate({ height: '0px' });
+        $(this).css('backgroundPosition', 'top left');
+        $("i", this).toggleClass("fa-caret-square-down fa-caret-square-up")
+        open = false;
+    }  
+});		
