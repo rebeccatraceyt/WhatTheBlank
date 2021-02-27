d3b52071fd12036835ca83c61cf675cefae82382
Rebecca Tracey-Timoney <71783662+rebeccatraceyt@users.noreply.github.com>
2021-02-27 18:29:38 +0000

    Initial setup of game mode

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,87 @@
 // --------------------------------------------------------- Variables
 let userName = localStorage.getItem("userName"); // Load username
 
+const question = document.getElementById('question');
+const answers = Array.from(document.getElementsByClassName('answer-btn'));
+
+let currentQuestion = {};
+let acceptingAnswers = false; // can't answer until everything loaded
+let score = 0;
+let questionCounter = 0;
+let availableQuestions = [];
+
+const tvQuestions = [
+    {
+        question: "I'll be there for you, when the rain starts to _ _ _ _",
+        answer1: 'fall',
+        answer2: 'pour',
+        answer3: 'rain',
+        correctAnswer: 2
+    },
+    {
+        question: "The license plate said fresh and it had _ _ _ _ in the mirror",
+        answer1: 'dice',
+        answer2: 'mice',
+        answer3: 'rice',
+        correctAnswer: 1
+    },
+    {
+        question: "Hanging out down the _ _ _ _ _, the same old thing we did last week",
+        answer1: 'beach',
+        answer2: 'street',
+        answer3: 'block',
+        correctAnswer: 2
+    }
+];
+
+const correctBonus = 10; // How much correct answer is correct
+const maxQuestions = 3; // How many questions before end
+
+startGame = () => {
+    questionCounter = 0;
+    score = 0;
+    availableQuestions = [...tvQuestions]; // copy from array - spread operator: spread each item into new array
+    getNewQuestion();
+}
+
+getNewQuestion = () => {
+    if(availableQuestions.length === 0 || questionCounter >=  maxQuestions){
+        // go to end page
+        return window.location.assign(); // RETURN END PAGE
+    }
+
+    questionCounter++; // start game and increment to 1
+    const questionIndex = Math.floor(Math.random() * availableQuestions.length); // amount is always equal to how many questions are left
+    currentQuestion = availableQuestions[questionIndex];
+    question.innerText = currentQuestion.question;
+
+    answers.forEach(answer => { // iterate through answers
+        const number = answer.dataset['number']; // get number from data set property - get data set and give me the number
+        answer.innerText = currentQuestion['answer' + number]; //grab answer property and data attribute associated to get the number
+    });
+
+    availableQuestions.splice(questionIndex, 1); // get rid of used question
+
+    acceptingAnswers = true; // allow user to answer
+}
+
+answers.forEach(answer => {
+    answer.addEventListener('click', e => {
+        if(!acceptingAnswers) return; // ignore click if not ready
+
+        acceptingAnswers = false;
+        const selectedChoice = e.target;
+        const selectedAnswer = selectedChoice.dataset['number'];
+
+        getNewQuestion();
+    });
+});
+
+startGame();
+
+
+
+
 const toggleSwitch = document.querySelector('.toggle-switch input[type="checkbox"]'); // toggles light/dark function 
 const currentTheme = localStorage.getItem('theme') ? localStorage.getItem('theme') : null; // gets users theme preference
 
@@ -43,36 +124,9 @@ function userData() {
 $('#tv-cat').on('click', function() {
     $('#welcome-message').hide();
     $('#question').show();
+    $('#question-btn').show();
     
-    const tvQuestions = [
-        {
-            question: "I'll be there for you, when the rain starts to _ _ _ _",
-            answers: {
-                a: 'fall',
-                b: 'pour',
-                c: 'rain'
-            },
-            correctAnswer: 'b'
-        },
-        {
-            question: "The license plate said fresh and it had _ _ _ _ in the mirror",
-            answers: {
-                a: 'dice',
-                b: 'mice',
-                c: 'rice'
-            },
-            correctAnswer: 'a'
-        },
-        {
-            question: "Hanging out down the _ _ _ _ _, the same old thing we did last week",
-            answers: {
-                a: 'beach',
-                b: 'street',
-                c: 'block'
-            },
-            correctAnswer: 'b'
-        }
-    ];
+    
     function shuffleTv(tvQuestions) {
         /* shuffles the questions order
             sourced and edited from https://javascript.info/task/shuffle */
@@ -83,7 +137,8 @@ $('#tv-cat').on('click', function() {
     }
     function resetTV() {
 
-    } 
+        }
+
 });
 
 $('#chart-cat').on('click', function() {
@@ -101,6 +156,7 @@ $('#movie-cat').on('click', function() {
 $('#logo-sec').on('click', function() {
     $('#welcome-message').show();
     $('#question').hide();
+    $('#question-btn').hide();
 });
 
 // --------------------------------------------------------- Light / Dark Mode Toggle
