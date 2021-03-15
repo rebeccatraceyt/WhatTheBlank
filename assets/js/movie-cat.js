1755e9a90401b2d6a4f305936f1b738dc1a8d950
Rebecca Tracey-Timoney <71783662+rebeccatraceyt@users.noreply.github.com>
2021-03-15 21:46:37 +0000

    Separated questions into .json  files

--- assets/js/movie-cat.js
+++ assets/js/movie-cat.js
@@ -22,78 +22,20 @@ let movieHighScore = 0;
 const correctBonus = 1; // How much correct answer is correct
 const maxQuestions = 10; // How many questions before end
 
-const movieQuestions = [
-    {
-        question: "What can I say except, 'You're welcome', for the tides, the sun, the _____",
-        answer1: 'stars',
-        answer2: 'sky',
-        answer3: 'moon',
-        correctAnswer: 2
-    },
-    {
-        question: "Go Greased Lightnin', you're coasting through the _____ lap trial",
-        answer1: 'heat',
-        answer2: 'pre',
-        answer3: 'beat',
-        correctAnswer: 1
-    },
-    {
-        question: "You just remember what your old _____ said, you've got a friend in me",
-        answer1: 'bud',
-        answer2: 'friend',
-        answer3: 'pal',
-        correctAnswer: 3
-    },
-    {
-        question: "Here come the Men in Black, Galaxy _____",
-        answer1: 'protectors',
-        answer2: 'defenders',
-        answer3: 'conquerors',
-        correctAnswer: 2 
-    },
-    {
-        question: "His palms are sweaty, knees weak, arms are _____",
-        answer1: 'weak',
-        answer2: 'tired',
-        answer3: 'heavy',
-        correctAnswer: 3
-    },
-    {
-        question: "Now I've had the time of my _____",
-        answer1: 'life',
-        answer2: 'night',
-        answer3: 'day',
-        correctAnswer: 1
-    },
-    {
-        question: "Tonight I gotta cut loose, footloose. Kick off your _____ shoes",
-        answer1: 'Sunday',
-        answer2: 'dancing',
-        answer3: 'Monday',
-        correctAnswer: 1
-    },
-    {
-        question: "Where troubles melt like lemon drops away above the chimney tops, that's where you'll find _____",
-        answer1: 'Obi',
-        answer2: 'me',
-        answer3: 'us',
-        correctAnswer: 2 
-    },
-    {
-        question: "As you _____ on by, will you call my name?",
-        answer1: 'run',
-        answer2: 'dance',
-        answer3: 'walk',
-        correctAnswer: 3
-    },
-    {
-        question: "Near, far, wherever you are, I believe that the _____ does go on",
-        answer1: 'heart',
-        answer2: 'soul',
-        answer3: 'love',
-        correctAnswer: 1 
-    }
-];
+let movieQuestions = [];
+
+fetch("assets/js/questions/movies-questions.json")
+    .then(res => {
+        return res.json();
+    })
+    .then(loadedQuestions => {
+        movieQuestions = loadedQuestions;
+        startGame();
+    })
+    .catch(err => {
+        console.error(err);
+    });
+
 
 function startGame () {
     scoreNumber.innerText = '0';
@@ -204,5 +146,3 @@ const incrementScore = num => {
     movieScore += num;
     scoreNumber.innerText = movieScore;
 };
-
-startGame(); 
--- assets/js/tv-cat.js
+++ assets/js/tv-cat.js
@@ -22,78 +22,19 @@ let movieHighScore = 0;
 const correctBonus = 1; // How much correct answer is correct
 const maxQuestions = 10; // How many questions before end
 
-const tvQuestions = [
-    {
-        question: "I'll be there for you, when the rain starts to _____",
-        answer1: 'fall',
-        answer2: 'pour',
-        answer3: 'lash',
-        correctAnswer: 2
-    },
-    {
-        question: "The license plate said fresh and it had _____ in the mirror",
-        answer1: 'dice',
-        answer2: 'mice',
-        answer3: 'rice',
-        correctAnswer: 1
-    },
-    {
-        question: "Hanging out down the _____, the same old thing we did last week",
-        answer1: 'beach',
-        answer2: 'street',
-        answer3: 'block',
-        correctAnswer: 2
-    },
-    {
-        question: "But I can't do this all on my own, no, I know, I'm no ______",
-        answer1: 'Superman',
-        answer2: 'Doctor',
-        answer3: 'Actor',
-        correctAnswer: 1
-    },
-    {
-        question: "Sometimes you want to go where everybody knows your _____",
-        answer1: 'age',
-        answer2: 'story',
-        answer3: 'name',
-        correctAnswer: 3
-    },
-    {
-        question: "Maths, science, history, unraveling the mysteries that all started with a big _____",
-        answer1: 'bang',
-        answer2: 'noise',
-        answer3: 'boom',
-        correctAnswer: 1 
-    },
-    {
-        question: "Scooby-Dooby-Doo, where are you? We got some _____ to do now",
-        answer1: 'jobs',
-        answer2: 'stuff',
-        answer3: 'work',
-        correctAnswer: 3
-    },
-    {
-        question: "Then drop on the deck and flop like a _____, Spongebob Squarepants!",
-        answer1: 'fish',
-        answer2: 'sponge',
-        answer3: 'star',
-        correctAnswer: 1
-    },
-    {
-        question: "Pokemon! Oh, you're my best friend, in a world we must _____!",
-        answer1: 'destroy',
-        answer2: 'defend',
-        answer3: 'understand',
-        correctAnswer: 2
-    },
-    {
-        question: "Catches _____ just like flies. Look out! Here comes the Spider-Man.",
-        answer1: 'dreams',
-        answer2: 'bees',
-        answer3: 'thieves',
-        correctAnswer: 3
-    }
-];
+let tvQuestions = [];
+
+fetch("assets/js/questions/tv-questions.json")
+    .then(res => {
+        return res.json();
+    })
+    .then(loadedQuestions => {
+        tvQuestions = loadedQuestions;
+        startGame();
+    })
+    .catch(err => {
+        console.error(err);
+    });
 
 function startGame () {
     scoreNumber.innerText = '0';
@@ -203,5 +144,3 @@ const incrementScore = num => {
     tvScore += num;
     scoreNumber.innerText = tvScore;
 };
-
-startGame(); 
